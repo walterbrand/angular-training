cfc553b258d285a0c9e4a4cbc17f08558f3e68a2
Ramin Mohammadi <ramstyles@gmail.com>
2014-06-30 13:59:03 +0200

    add firstName and lastName

--- app/scripts/controllers/formcontroller.js
+++ app/scripts/controllers/formcontroller.js
@@ -2,7 +2,9 @@ angular.module('app').controller('formcontroller', ['$scope', function($scope){
     'use strict';
     $scope.data = {
         attempts : 0,
-        complete : false
+	complete : false,
+	    firstName: '',
+	    lastName: ''
     };
 
     $scope.submit = function(){
