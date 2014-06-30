320f1a3a414e4db72fd7e05daf4d86358eb971df
Ramin Mohammadi <ramstyles@gmail.com>
2014-06-30 14:13:59 +0200

    added various input examples for amount

--- app/scripts/controllers/formcontroller.js
+++ app/scripts/controllers/formcontroller.js
@@ -1,13 +1,65 @@
-angular.module('app').controller('formcontroller', ['$scope', function($scope){
-    'use strict';
-    $scope.data = {
-        attempts : 0,
-	complete : false,
-	    firstName: '',
-	    lastName: ''
-    };
+angular.module('app').controller('formcontroller', ['$scope', function ($scope) {
+	'use strict';
+	$scope.data = {
+		attempts: 0,
+		complete: false,
+		firstName: '',
+		lastName: ''
+	};
+	$scope.transaction = {
+		fromAccountRef: '',
+		toAccountRef: '',
+		transactionRef: '',
+		amount: 0,
+		description: ''
+	};
 
-    $scope.submit = function(){
-        this.data.attempts++;
-    }
+	$scope.onChangeAmount = function () {
+		console.log('onChangeAmount: %o', arguments);
+	};
+
+	$scope.submit = function () {
+		if ($scope.data.firstName.length > 0 && $scope.data.lastName.length > 0) {
+			$scope.data.attempts++;
+			$scope.data.complete = true;
+		} else {
+			$scope.data.complete = false;
+		}
+	};
+}]);
+angular.module('app').directive('moneyInput', function () {
+	return {
+		restrict: 'E',
+		template: '<div  >' +
+			'<span>&euro;</span>' +
+			'<span contenteditable="true" >{{transaction.amount | currencyNoFraction}}</span>' +
+			'<span style="font-size: xlarge; margin-right: 5px; ">,</span>' +
+			'<span contenteditable="true">{{transaction.amount | currencyFraction}}</span>' +
+			'</div>'
+	};
+});
+
+angular.module('app').filter('currencyFraction', [ '$filter', function ($filter) {
+	var currency = $filter('currency'),
+		formats = {
+			CURRENCY_SYM: "$",
+			DECIMAL_SEP: ".",
+			GROUP_SEP: ","
+		};
+	return function (amount, symbol) {
+		var value = currency(amount, symbol).split(formats.DECIMAL_SEP);
+		return value[1] || '';
+	}
+}]);
+angular.module('app').filter('currencyNoFraction', [ '$filter', function ($filter) {
+	var currency = $filter('number'),
+		formats = {
+			CURRENCY_SYM: '$',
+			DECIMAL_SEP: '.',
+			GROUP_SEP: ','
+		};
+	return function (amount) {
+		var value = currency(amount).split(formats.DECIMAL_SEP)[0];
+		return value;
+	}
 }]);
--- karma.conf.js
+++ karma.conf.js
@@ -11,9 +11,9 @@ module.exports = function (config) {
 
         // list of files / patterns to load in the browser
         files: [
-            'app/bower_components/jquery/jquery.js',
-            'app/bower_components/angular/angular.js',
-            'app/bower_components/angular-mocks/angular-mocks.js',
+            'bower_components/jquery/jquery.js',
+            'bower_components/angular/angular.js',
+            'bower_components/angular-mocks/angular-mocks.js',
             //'app/scripts/*.js',
             'app/scripts/**/*.js',
             //'test/mock/**/*.js',
@@ -49,7 +49,7 @@ module.exports = function (config) {
         // - Safari (only Mac)
         // - PhantomJS
         // - IE (only Windows)
-        browsers: ['PhantomJS'],
+        browsers: ['PhantomJS', 'Safari'],
 
 
         // Continuous Integration mode
